2b90d660874f7f640482f88b77cab27ce11e484c
Jeremie Miller <jeremie.miller@gmail.com>
2021-07-29 16:15:05 -0600

    some cleanups and fixes

--- bbs_all.js
+++ bbs_all.js
@@ -23,7 +23,7 @@ const { GeneralSign } = require('jose/jws/general/sign');
     jwk.crv = 'Bls12381G2';
     jwk.x = Buffer.from(keyPair.publicKey).toString("base64url");
     jwk.kid = await calculateThumbprint(jwk);
-    jwk.alg = 'BBS+';
+    jwk.alg = 'BBS';
     jwk.use = 'proof';
     jwk.payloads = [
         {"claims":["family_name", "given_name"]},
@@ -37,20 +37,25 @@ const { GeneralSign } = require('jose/jws/general/sign');
     protected.kid = jwk.kid;
     protected.issuer = 'https://issuer.tld';
     protected.typ = 'JOSE+Proof';
-    jwp.protected = encode(JSON.stringify(protected));
-    const payload_buffs = [
+    const protected_buff = Buffer.from(JSON.stringify(protected), 'utf8');
+    jwp.protected = encode(protected_buff);
+    const payloads_buff = [
         Buffer.from(JSON.stringify({'family_name':'Miller','given_name':'Jer'}), "utf8"),
         Buffer.from(JSON.stringify({'email':'jer@jeremie.com'}), "utf8"),
         Buffer.from(JSON.stringify({'birthdate':'2042'}), "utf8")
     ];
-    jwp.payloads = payload_buffs.map(encode);
+    jwp.payloads = payloads_buff.map(encode);
     
+    let messages = [];
+    messages.push(Uint8Array.from(protected_buff));
+    messages.concat(payloads_buff.map((item)=>Uint8Array.from(item)));
     const signature = await blsSign({
         keyPair,
-        messages: payload_buffs.map((item)=>Uint8Array.from(item))
+        messages
     });
   
     jwp.proof = encode(signature);
     console.log(JSON.stringify(jwp));
+
 
 })();
