4f8466e44ae1577bae16f4603aa8113237362b6d
Jeremie Miller <jeremie.miller@gmail.com>
2021-07-29 16:06:38 -0600

    basics working for bbs+ jwk and jwp issuance

---
+++ bbs_all.js
@@ -0,0 +1,56 @@
+const {
+    generateBls12381G2KeyPair,
+    blsSign,
+    blsVerify,
+    blsCreateProof,
+    blsVerifyProof,
+  } = require('@mattrglobal/bbs-signatures');
+
+const { fromKeyLike } = require('jose/jwk/from_key_like');
+const { generateKeyPair } = require('jose/util/generate_key_pair');
+const { calculateThumbprint } = require('jose/jwk/thumbprint');
+const { encode, decode } = require('jose/util/base64url');
+const { readFileSync } = require('fs');
+const { GeneralSign } = require('jose/jws/general/sign');
+
+
+(async function(){
+    // generate the long-term public key
+    const keyPair = await generateBls12381G2KeyPair();
+
+    const jwk = {};
+    jwk.kty = 'OKP';
+    jwk.crv = 'Bls12381G2';
+    jwk.x = Buffer.from(keyPair.publicKey).toString("base64url");
+    jwk.kid = await calculateThumbprint(jwk);
+    jwk.alg = 'BBS+';
+    jwk.use = 'proof';
+    jwk.payloads = [
+        {"claims":["family_name", "given_name"]},
+        {"claims":["email"]},
+        {"claims":["birthdate"]}]
+    console.log(JSON.stringify(jwk));
+
+    // generate jwp
+    const jwp = {};
+    const protected = {};
+    protected.kid = jwk.kid;
+    protected.issuer = 'https://issuer.tld';
+    protected.typ = 'JOSE+Proof';
+    jwp.protected = encode(JSON.stringify(protected));
+    const payload_buffs = [
+        Buffer.from(JSON.stringify({'family_name':'Miller','given_name':'Jer'}), "utf8"),
+        Buffer.from(JSON.stringify({'email':'jer@jeremie.com'}), "utf8"),
+        Buffer.from(JSON.stringify({'birthdate':'2042'}), "utf8")
+    ];
+    jwp.payloads = payload_buffs.map(encode);
+    
+    const signature = await blsSign({
+        keyPair,
+        messages: payload_buffs.map((item)=>Uint8Array.from(item))
+    });
+  
+    jwp.proof = encode(signature);
+    console.log(JSON.stringify(jwp));
+
+})();
